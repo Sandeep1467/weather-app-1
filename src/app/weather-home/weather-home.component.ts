165b75720d1e97bf2f3c2767c4837ef34d5618f2
neha <neha.kumari732@gmail.com>
2021-10-09 13:52:15 +0530

    commit error

--- src/app/weather-home/weather-home.component.ts
+++ src/app/weather-home/weather-home.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscriber } from 'rxjs';
 import { WeatherClientService } from '../weather-client.service';
-
-
+import {  AfterViewInit, ViewChild, ElementRef } from 
+'@angular/core';
+//import { } from '@types/googlemaps';
+import {} from '@agm/core/services/google-maps-types';
+@ViewChild('mapContainer', {static: false}) gmap: ElementRef;
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
