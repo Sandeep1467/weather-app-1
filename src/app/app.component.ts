99fea04bd46ac472236e727dd6e3d10ab7bcad6f
neha <neha.kumari732@gmail.com>
2021-10-09 16:52:51 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-
+import { AfterViewInit, ViewChild, ElementRef } from 
+  '@angular/core';
+ 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +9,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather-app';
+  @ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+  map: google.maps.Map;
  
+    lat = 40.73061;
+    lng = -73.935242;
+
+    coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+    mapOptions: google.maps.MapOptions = {
+     center: this.coordinates,
+     zoom: 8
+    };
+
+    marker = new google.maps.Marker({
+      position: this.coordinates,
+      map: this.map,
+    });
+
+    ngAfterViewInit() {
+      this.mapInitializer();
+    }
+
+    mapInitializer() {
+      this.map = new google.maps.Map(this.gmap.nativeElement, 
+      this.mapOptions);
+      this.marker.setMap(this.map);
+    }
 }
+
+
--- src/app/weather-home/weather-home.component.ts
+++ src/app/weather-home/weather-home.component.ts
@@ -3,9 +3,7 @@ import { Subscriber } from 'rxjs';
 import { WeatherClientService } from '../weather-client.service';
 import {  AfterViewInit, ViewChild, ElementRef } from 
 '@angular/core';
-//import { } from '@types/googlemaps';
-import {} from '@agm/core/services/google-maps-types';
-@ViewChild('mapContainer', {static: false}) gmap: ElementRef;
+
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
@@ -15,6 +13,7 @@ export class WeatherHomeComponent implements OnInit {
   
   public city: string = "";
   weather: any
+
 
   constructor(private weatherClient: WeatherClientService) { }
 
