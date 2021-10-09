2609f09223e554bc6cbf3bb08f8161031ce79865
nehakumari0201 <47860946+nehakumari0201@users.noreply.github.com>
2021-10-09 17:03:37 +0530

    Update weather-home.component.ts

--- src/app/weather-home/weather-home.component.ts
+++ src/app/weather-home/weather-home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscriber } from 'rxjs';
 import { WeatherClientService } from '../weather-client.service';
-import {  AfterViewInit, ViewChild, ElementRef } from 
+//import {  AfterViewInit, ViewChild, ElementRef } from 
 '@angular/core';
 
 @Component({
